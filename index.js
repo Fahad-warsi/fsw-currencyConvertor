e86fe310af97e70c40538711fa42a3d351623260
Fahad warsi <fahad7swarsi@gmail.com>
2024-05-05 09:43:22 +0500

    mini project-Currency conertor

---
+++ index.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1, //base
+    PKR: 278,
+    AED: 3.67,
+    EURO: 0.93,
+};
+const answer = await inquirer.prompt([
+    {
+        message: "Select from currency : ",
+        type: "list",
+        choices: ["USD", "PKR", "AED", "EURO"],
+        name: "From",
+    },
+    {
+        message: "Select TO currency : ",
+        type: "list",
+        choices: ["USD", "PKR", "AED", "EURO"],
+        name: "TO",
+    },
+    {
+        message: "Enter your amount : ",
+        type: "number",
+        name: "amount",
+    },
+]);
+let fromAmount = currency[answer.From];
+let toAmount = currency[answer.TO];
+let amount = answer.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
+//console.log(fromAmount, toAmount, answer.amount);
